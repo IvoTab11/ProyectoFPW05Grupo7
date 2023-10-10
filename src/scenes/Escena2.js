be27030e358850ce26bed225f0c082e61c68d512
ValdezCruzLean <leancruz027@gmail.com>
2023-10-10 13:06:25 -0300

    se agrego el nivel inicial junto a los enemigos

--- src/scenes/Escena2.js
+++ src/scenes/Escena2.js
@@ -2,16 +2,21 @@ class Escena2 extends Phaser.Scene{
     constructor(){
         super("Escena2");
         this.platforms = null;
+        this.lifeText="";
+        this.life= 100 ;
+        this.scoreText="";
+        this.score=0;
+       
     }
 
     preload(){
         this.load.image('sky', '../public/img/sky.png')
-       // this.c/load.image('nave', '../public/img/nave.png', {frameWidth: 200, frameHeight: 200})
         this.load.spritesheet('nave', '../public/img/nave.png', { frameWidth: 70, frameHeight: 62 });
         this.load.image('red', '../public/img/red.png')
+        this.load.image('bullet', '../public/img/shoot.png')
+        this.load.image('enemy', '../public/img/enemy.png')
 
     }
-
     create(){
         this.add.image(400, 300, 'sky');
         let particles = this.add.particles(0,0,'red',{
@@ -20,64 +25,126 @@ class Escena2 extends Phaser.Scene{
             scale:{start:1,end:0},
             blendMode: 'ADD'
         });
-        this.player = this.physics.add.sprite(100,100,'nave');
-        particles.startFollow(this.player);
-        
-        this.anims.create({
-            key: 'left',
-            frames: this.anims.generateFrameNumbers('nave', { start: 0, end: 0 }),
-            frameRate: 10,
-            repeat: -1
-            });
-            this.anims.create({
-            key: 'down',
-            frames: [ { key: 'nave', frame: 1 } ],
-            frameRate: 20
-            });
-            this.anims.create({
-            key: 'right',
-            frames: this.anims.generateFrameNumbers('nave', { start: 0, end: 0 }),
-            frameRate: 10,
-            repeat: -1
-            });
-            this.anims.create({
-                key: 'up',
-                frames: this.anims.generateFrameNumbers('nave', { start: 2, end: 2 }),
-                frameRate: 10,
-                repeat: -1
-                });
-        
 
-            this.cursors = this.input.keyboard.createCursorKeys();
+   
+    this.player = this.physics.add.sprite(100,100,'nave');
+    particles.startFollow(this.player);
+    this.Shoot = this.physics.add.group();
+    this.time.addEvent({ //Indica la accion que se repetira cada 1500 milisegundos
+        delay: 1500,
+        callback: () => {
+            this.createEnemies();
+        },
+        callbackScope: this,
+        repeat: -1,
+    });
             
+    this.anims.create({//Crea las animaciones del personaje
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('nave', { start: 0, end: 0 }),
+        frameRate: 10,
+        repeat: -1
+        });
+        this.anims.create({
+        key: 'down',
+        frames: [ { key: 'nave', frame: 1 } ],
+        frameRate: 20
+        });
+        this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('nave', { start: 0, end: 0 }),
+        frameRate: 10,
+        repeat: -1
+        });
+        this.anims.create({
+            key: 'up',
+            frames: this.anims.generateFrameNumbers('nave', { start: 2, end: 2 }),
+            frameRate: 10,
+            repeat: -1
+            });
+
+    this.cursors = this.input.keyboard.createCursorKeys();
         
+    //Ubica y define el color y el tamaño de el puntaje y la vida del player en la pantalla 
+    this.lifeText = this.add.text(16, 16, 'life:' + this.life , { fontSize: '24px', fill: '#000' }); 
+    this.scoreText = this.add.text(16, 40, 'score: 0', { fontSize: '24px', fill: '#000' }); 
 
-    }
+    this.input.keyboard.on('keydown-A', event =>//Indicamos que si se presiona la tecla 'A' el player va a disparar
+    {
+        this.Shoot.create(this.player.x,this.player.y,'bullet').setVelocityX(300);
+    });
 
-     update() {
-        if (this.cursors.left.isDown) {
-         this.player.setVelocityX(-160);
-         this.player.anims.play('left', true);
-         }
-         else if (this.cursors.right.isDown) {
-         this.player.setVelocityX(160);
-         this.player.anims.play('right', true);
-         }
-         else if (this.cursors.up.isDown ) {
-         this.player.setVelocityY(-160);
-         this.player.anims.play('up');
-         }
-         else if(this.cursors.down.isDown){
+}
+
+    createEnemies() {// Creador de enemigos, actualiza su posicion y los destruye
+        let enemyGroup = this.physics.add.group();
+        let enemiesHorizontalDistance = 790;
+        for (let i = 0; i < 7; i++){
+            let enemiesHeightPosition= Phaser.Math.Between(20,580);
+            let enemies = enemyGroup.create(enemiesHorizontalDistance, enemiesHeightPosition, 'enemy');
+            this.enemiesHorizontalDistance = enemiesHorizontalDistance + 300;
+            enemies.body.velocity.x = -150;
+            enemies.checkWorldBounds=true;
+            enemies.on('outOfBounds',() =>{
+            enemies.destroy();
+                });
+             }
+            //Detecta la colision entre el player y las enemigos y la colision entre las balas y los enemigos
+            this.physics.add.overlap(this.player, enemyGroup, this.impact, null, this);
+            this.physics.add.overlap(this.Shoot, enemyGroup, this.collideShoot, null, this); 
+        }
+
+
+
+         update() {//Actualizacion de la posicion del personaje con su respectiva animacion
+         if (this.cursors.left.isDown) {
+            this.player.setVelocityX(-160);
+             this.player.anims.play('left', true);
+                 }
+             else if (this.cursors.right.isDown) {
+            this.player.setVelocityX(160);
+            this.player.anims.play('right', true);
+                }
+            else if (this.cursors.up.isDown ) {
+            this.player.setVelocityY(-160);
+            this.player.anims.play('up');
+                }
+            else if(this.cursors.down.isDown){
             this.player.setVelocityY(160);
             this.player.anims.play('down');
-         }
-         else{
+                }
+            else{
             this.player.setVelocityY(0);
             this.player.setVelocityX(0);
             this.player.anims.play('right');
-         }
-         this.player.setCollideWorldBounds(true);
-         }
-
+                }
+     this.player.setCollideWorldBounds(true);
+ 
 }
+
+
+
+
+        collideShoot(Shoot, enemies) { //Colisión entre el disparo y los enemigos
+            Shoot.disableBody(true, true);
+            enemies.disableBody(true,true);
+            this.score += 10;
+            this.scoreText.setText('Score: ' + this.score);
+            if(this.score > 250){
+            this.scene.start('Escena3'); //Celi esta pasa al level de Nico osea no la toques D:<
+
+              }
+        }
+
+        impact(player, enemies){  // Colisión entre el player y los enemigos
+         enemies.disableBody(true,true);
+            this.life -= 25;
+            this.lifeText.setText('life: ' + this.life + '%');
+            if(this.life <= 0){
+            this.scene.start('Escena3');//Celi Aca va la escena de que perdio :/
+                  }
+            }       
+}
+
+
 export default Escena2;
